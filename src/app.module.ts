8cf76595bf1202c9178f2c6ec8576a0e9cf7f392
codersskil <161935014+codersskil@users.noreply.github.com>
2025-01-16 21:16:24 +0530

    ingestion module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DocumentModule } from './document/document.module';
+import { IngestionModule } from './ingestion/ingestion.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DocumentModule],
+  imports: [AuthModule, UsersModule, DocumentModule, IngestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
