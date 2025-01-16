aad0f2613c66584b34cce920824b8ed7b9e277ee
codersskil <161935014+codersskil@users.noreply.github.com>
2025-01-16 21:20:42 +0530

    config module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DocumentModule } from './document/document.module';
 import { IngestionModule } from './ingestion/ingestion.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DocumentModule, IngestionModule],
+  imports: [AuthModule, UsersModule, DocumentModule, IngestionModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
